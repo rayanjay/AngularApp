8324dcbc2ea3d42cd0eab22931eb1ed7f4d13a8d
Rayan <jangdar2@gator.uhd.edu>
2021-07-22 12:12:01 -0500

    Add existing file

---
+++ angular-app/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {HttpClientModule}from '@angular/common/http';
+import{FormsModule} from '@angular/forms';
+import{RouterModule, Routes} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+
+const appRoutes: Routes=[
+  {path: '',component:TasksComponent}
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonsComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule
+    .forRoot(appRoutes,{enableTracing:true})
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-app/src/app/components/buttons/buttons.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons',
+  templateUrl: './buttons.component.html',
+  styleUrls: ['./buttons.component.css']
+})
+export class ButtonsComponent implements OnInit {
+
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+  @Output() btnClick = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(){
+    this.btnClick.emit();
+  }
+}
